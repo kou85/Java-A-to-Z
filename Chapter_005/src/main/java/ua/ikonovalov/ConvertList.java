d746d01e8537c07ac138d9002ef75706a88985cc
Ivan Konovalov <kou85@mail.ru>
2017-05-25 21:56:53 +0300

    convert List

--- Chapter_005/src/main/java/ua/ikonovalov/ConvertList.java
+++ Chapter_005/src/main/java/ua/ikonovalov/ConvertList.java
@@ -1,10 +1,13 @@
 package ua.ikonovalov;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
- * Created by Strong on 15.05.17.
+ * @autor Konovalov
+ * @since 25.05.17
+ * @version 1.0
  */
 public class ConvertList {
     public List<Integer> toList (int[][] array) {
@@ -17,6 +20,37 @@ public class ConvertList {
         return arrayList;
     }
     public int[][] toArray (List<Integer> list, int rows) {
+        int length = list.size() / rows;
+        if (list.size() % rows != 0) {
+            length += 1;
+        }
+        int[][] listArray = new int[rows][length];
+        Iterator<Integer> iterator = list.iterator();
+        for (int row = 0; row != rows; row++) {
+            for (int column = 0; column != length; column++) {
+                if (iterator.hasNext()) {
+                    listArray[row][column] = iterator.next();
+                } else {
+                    break;
+                }
+            }
+        }
+        return listArray;
 
+    }
+
+    /**
+     * Method converts the sheet arrays in a single Integer worksheet.
+     * @param list collection.
+     * @return collection.
+     */
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> addArray = new ArrayList<>();
+        for (int[] index : list) {
+            for (int value : index) {
+                addArray.add(value);
+            }
+        }
+        return addArray;
     }
 }
