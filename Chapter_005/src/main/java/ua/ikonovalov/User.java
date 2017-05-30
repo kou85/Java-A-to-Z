235c102deb9fb8291a3d2b5b7e13a9f49c9cd2f7
Ivan Konovalov <kou85@mail.ru>
2017-05-30 22:42:27 +0300

    userConvert

--- Chapter_005/src/main/java/ua/ikonovalov/User.java
+++ Chapter_005/src/main/java/ua/ikonovalov/User.java
@@ -1,7 +1,81 @@
 package ua.ikonovalov;
 
 /**
- * Created by Strong on 30.05.17.
+ * @autor Konovalov.
+ * @since 30.05.17.
+ * @version 1.0/
  */
 public class User {
+    /**
+     *
+     */
+    private int id;
+    /**
+     *
+     */
+    private String name;
+    /**
+     *
+     */
+    private String city;
+
+    /**
+     *
+     * @param id
+     * @param name
+     * @param city
+     */
+    public User(int id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getId (){
+        return this.id;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCity() {
+        return this.city;
+    }
+
+    /**
+     *
+     * @param city
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
--- Chapter_005/src/main/java/ua/ikonovalov/UserConvert.java
+++ Chapter_005/src/main/java/ua/ikonovalov/UserConvert.java
@@ -1,7 +1,25 @@
 package ua.ikonovalov;
 
+import java.util.*;
+
 /**
- * Created by Strong on 30.05.17.
+ * @autor Konovalov.
+ * @since 30.05.17.
+ * @version 1.0/
  */
 public class UserConvert {
+    /**
+     *
+     * @param list
+     * @return
+     */
+    public HashMap<Integer, User>process(List<User> list){
+        HashMap<Integer, User> userMap= new HashMap<>();
+            for(User user : list) {
+                userMap.put(user.getId(), user);
+            }
+
+        return userMap;
+
+    }
 }
