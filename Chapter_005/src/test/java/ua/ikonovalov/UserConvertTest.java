34105ac04e429aa4a01132a2a0ae3f39c8e56157
Ivan Konovalov <kou85@mail.ru>
2017-06-08 22:39:07 +0300

    test

---
+++ Chapter_005/src/test/java/ua/ikonovalov/UserConvertTest.java
@@ -0,0 +1,16 @@
+package ua.ikonovalov;
+
+import org.junit.Test;
+
+/**
+ * Created by Strong on 08.06.17.
+ */
+public class UserConvertTest {
+
+    @Test
+    public void userConvertToMap(){
+
+        UserConvert user = new UserConvert();
+
+    }
+}
