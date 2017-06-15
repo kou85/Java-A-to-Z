2286a15b8c26c01b151f7fc47b0007f8f5df06f2
Ivan Konovalov <kou85@mail.ru>
2017-06-15 23:20:53 +0300

    test

--- Chapter_005/src/test/java/ua/ikonovalov/UserConvertTest.java
+++ Chapter_005/src/test/java/ua/ikonovalov/UserConvertTest.java
@@ -3,6 +3,12 @@ package ua.ikonovalov;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by Strong on 08.06.17.
  */
@@ -19,19 +25,36 @@ public class UserConvertTest {
     int userThreeId = 2;
     String userThreeName = "userThreeName";
     String userThreeCity = "userThreeCity";
-
+    private List<User> testListUser;
+    private UserConvert testConvert;
 
     @Before
     public void infoTest() {
         userOne = new User(userOneId, userOneName, userOneCity);
         userTwo = new User(userTwoId, userTwoName, userTwoCity);
         userThree = new User(userThreeId, userThreeName, userThreeCity);
+        testListUser = new ArrayList<>();
+        testConvert = new UserConvert();
 
     }
+
+    private HashMap<Integer, User> expectedResult() {
+        HashMap<Integer, User> expectedHashMap = new HashMap<>();
+        expectedHashMap.put(userOne.getId(), userOne);
+        expectedHashMap.put(userTwo.getId(), userTwo);
+        expectedHashMap.put(userThree.getId(), userThree);
+        return expectedHashMap;
+
+    }
+
     @Test
     public void userConvertToMap(){
 
-        UserConvert user = new UserConvert();
+        testListUser.add(userOne);
+        testListUser.add(userTwo);
+        testListUser.add(userThree);
+        assertThat(expectedResult(), is(testConvert.process(testListUser)));
+
 
     }
 }
