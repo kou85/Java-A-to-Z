23a242f2f4e210138b078b1c27a0f9a2f1d7f0f6
Ivan Konovalov <kou85@mail.ru>
2017-10-08 23:03:58 +0300

    Collection sort

--- Chapter_005/src/main/java/ua/ikonovalov/Sorting/SortUser.java
+++ Chapter_005/src/main/java/ua/ikonovalov/Sorting/SortUser.java
@@ -1,7 +1,31 @@
 package ua.ikonovalov.Sorting;
 
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
- * Created by Strong on 13.09.17.
+ * Class SortUser.
+ *
+ * @author ru.sbulygin.
+ * @since 25.04.2017.
+ * @version 1.0.
  */
 public class SortUser {
+
+    /**
+     * The method sorts the list of users by age and returns the TreeSet collection.
+     *
+     * @param userList the list of users.
+     * @return TreeSet collection with users.
+     */
+    public Set<User> sort(List<User> userList) {
+        Set<User> userSet = new TreeSet<>();
+        for (User user : userList) {
+            userSet.add(user);
+        }
+        return userSet;
+    }
 }
+
