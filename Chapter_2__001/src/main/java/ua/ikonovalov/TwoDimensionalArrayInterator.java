530612d6db858961667ad43dbab8362e045500b3
Ivan Konovalov <kou85@mail.ru>
2018-02-21 18:42:22 +0200

    Interator

---
+++ Chapter_2__001/src/main/java/ua/ikonovalov/TwoDimensionalArrayInterator.java
@@ -0,0 +1,22 @@
+package main.java.ua.ikonovalov;
+
+import java.util.Iterator;
+/**
+ * Created by Strong on 21.02.18.
+ */
+public class TwoDimensionalArrayInterator implements Iterator{
+
+    public TwoDimensionalArrayInterator() {
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
+}
