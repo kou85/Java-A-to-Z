c0875c603408d0c05e3a9549a95b178b3e6cfced
Ivan Konovalov <kou85@mail.ru>
2018-03-05 22:26:49 +0200

    Interator

--- Chapter_2__001/src/main/java/ua/ikonovalov/TwoDimensionalArrayInterator.java
+++ Chapter_2__001/src/main/java/ua/ikonovalov/TwoDimensionalArrayInterator.java
@@ -5,18 +5,35 @@ import java.util.Iterator;
  * Created by Strong on 21.02.18.
  */
 public class TwoDimensionalArrayInterator implements Iterator{
+    private int lineArray;
+    private int columnArray;
+    public int [][] array;
 
-    public TwoDimensionalArrayInterator() {
+
+    public TwoDimensionalArrayInterator(int[][] array) {
+        this.array = array;
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        boolean result = true;
+
+        if (lineArray < 0 || columnArray < 0 || array.length <=  lineArray || array[0].length <= columnArray) {
+           result = false;
+        }
+        return result;
     }
 
     @Override
     public Object next() {
-        return null;
+
+        int result = array[lineArray][columnArray];
+        if (columnArray == array[lineArray].length - 1) {
+            lineArray += lineArray;
+        }
+        columnArray++;
+
+        return result;
     }
 }
-}
+
