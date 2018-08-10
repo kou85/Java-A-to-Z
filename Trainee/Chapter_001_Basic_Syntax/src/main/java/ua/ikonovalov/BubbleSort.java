60834b1cd9405b0c035956dc727add82394baef9
kou85 <kou85@mail.ru>
2018-08-10 10:34:57 +0300

    BubbleSort process

--- Trainee/Chapter_001_Basic_Syntax/src/main/java/ua/ikonovalov/BubbleSort.java
+++ Trainee/Chapter_001_Basic_Syntax/src/main/java/ua/ikonovalov/BubbleSort.java
@@ -6,6 +6,12 @@ package ua.ikonovalov;
 public class BubbleSort {
 
     public int[] sort(int[] array) {
-
+for (int i = 0; i<array.length-1; i++) {
+    if (array[i] > array[i+1]) {
+     int temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+    }
+} return array;
     }
 }
