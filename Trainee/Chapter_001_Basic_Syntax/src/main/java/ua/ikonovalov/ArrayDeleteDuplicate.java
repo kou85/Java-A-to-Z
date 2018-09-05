e87449eb9bf0004b4a8aa9c11b829ae3c36e089c
Ivan Konovalov <kou85@mail.ru>
2018-09-06 00:21:43 +0300

    ArrayDeleteDuplicate+Test and finish  Basic sintax!!

---
+++ Trainee/Chapter_001_Basic_Syntax/src/main/java/ua/ikonovalov/ArrayDeleteDuplicate.java
@@ -0,0 +1,36 @@
+package ua.ikonovalov;
+
+import java.util.Arrays;
+
+/**
+ * Created by Strong on 30.08.18.
+ */
+public class ArrayDeleteDuplicate {
+    /**
+     *
+     * @param array
+     * @return
+     */
+    public String[] removeDuplicate(String[] array) {
+       int countDublicates = 0;
+        for (int i = 0; i < array.length-1; i++) {
+            for (int k = i + 1; k < array.length; k++) {
+                if (array[i] != null && array[k] != null && array[i].equals(array[k])) {
+                    array[k] = null;
+                    countDublicates++;
+                }
+            }
+            for (int n = array.length - 1; n >= 0; n--) {
+                for (int j = 0; j < n; j++) {
+                    if (array[j] == null) {
+                        array[j] = array[j + 1];
+                        array[j + 1] = null;
+                    }
+                }
+            }
+        }
+        return Arrays.copyOf(array, array.length - countDublicates);
+    }
+
+}
+
---
+++ Trainee/Chapter_001_Basic_Syntax/src/test/java/ua/ikonovalov/ArrayDeleteDuplicateTest.java
@@ -0,0 +1,20 @@
+package ua.ikonovalov;
+
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Created by Strong on 05.09.18.
+ */
+public class ArrayDeleteDuplicateTest {
+
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDeleteDuplicate arrayDeleteDuplicate = new ArrayDeleteDuplicate();
+        String [] input = new String[] {"Петя", "Вася", "Петя", "Иван", "Петя"};
+        String [] result = arrayDeleteDuplicate.removeDuplicate(input);
+        String [] expect = new String[] {"Петя", "Вася", "Иван"};
+        assertThat(result, is(expect));
+    }
+}
