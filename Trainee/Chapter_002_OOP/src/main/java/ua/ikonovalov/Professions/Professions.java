cbaecb540642a1884cde93c853a50075523906ab
Ivan Konovalov <kou85@mail.ru>
2018-09-11 00:34:56 +0300

    Professions chapter 2 process

--- Trainee/Chapter_002_OOP/src/main/java/ua/ikonovalov/Professions/Professions.java
+++ Trainee/Chapter_002_OOP/src/main/java/ua/ikonovalov/Professions/Professions.java
@@ -7,7 +7,68 @@ public class Professions {
     /**
      *
      */
-    public String name;
-    public String profession;
+    private String name;
+    private String specialization;
+    private int experience;
+    private int salary;
 
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getSpecialization() {
+        return this.specialization;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getExperience() {
+        return this.experience;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @param specialization
+     */
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    /**
+     *
+     * @param experience
+     */
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    /**
+     *
+     * @param salary
+     */
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
