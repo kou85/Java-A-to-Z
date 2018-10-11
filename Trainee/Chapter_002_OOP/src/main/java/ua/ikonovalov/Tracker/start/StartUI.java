9406224fc17b6070f33722c746a80cd2868c2b5a
Konovalov Ivan <kou85@mail.ru>
2018-10-11 23:01:26 +0300

    process class StartUI

--- Trainee/Chapter_002_OOP/src/main/java/ua/ikonovalov/Tracker/start/StartUI.java
+++ Trainee/Chapter_002_OOP/src/main/java/ua/ikonovalov/Tracker/start/StartUI.java
@@ -8,6 +8,14 @@ import ua.ikonovalov.Tracker.models.*;
 public class StartUI {
     private Input input;
 
+    private static final String ADD = "0";
+    private static final String SHOW = "1";
+    private static final String EDIT = "2";
+    private static final String DELETE = "3";
+    private static final String FINE_BY_ID = "4";
+    private static final String FINE_BY_NAME = "5";
+    private static final String EXIT = "6";
+
     public StartUI(Input input) {
         this.input = input;
     }
