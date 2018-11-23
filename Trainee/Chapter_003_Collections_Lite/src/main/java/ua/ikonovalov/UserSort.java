ca85faa3fe56c8a3db300c33032f4df1885e4ad8
Ivan Konovalov <kou85@mail.ru>
2018-11-24 00:25:20 +0200

    add class UserSort+Tests: Sorted for name, age, nameLength used comporator

--- Trainee/Chapter_003_Collections_Lite/src/main/java/ua/ikonovalov/UserSort.java
+++ Trainee/Chapter_003_Collections_Lite/src/main/java/ua/ikonovalov/UserSort.java
@@ -1,17 +1,56 @@
 package ua.ikonovalov;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Created by Strong on 22.11.18.
+ * class UserSort
+ * @author Konovalov
+ * @version 1.0
+ * @since 24.11.18
  */
-public class UserSort {
-    Set<User> sort(List<User> o) {
-         //второй вариант копирования из эррей в сет
-       /* Set<User> user1 = new TreeSet<>();
-        user1.addAll(o);*/
-        return new TreeSet<User>(o);
+    public class UserSort {
+    /**
+     * Method add users to ArrayList converts to Set and sorts Age
+     */
+    public Set<User> sort(List<User> o) {
+             //второй вариант копирования из эррей в сет
+           /* Set<User> user1 = new TreeSet<>();
+            user1.addAll(o);*/
+            return new TreeSet<User>(o);
+    }
+    /**
+     * Method add users to ArrayList and sorts the length of the name
+     */
+    public List<User> sortNameLength (List<User> o) {
+        o.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getName().length()-o2.getName().length();
+                    }
+                }
+               );
+        return o;
+    }
+    /**
+     * Method add users to ArrayList and sorts the name and after the Age
+     */
+    public List<User> sortByAllFields (List<User> o) {
+        o.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int result = o1.getName().compareTo(o2.getName());
+                        if (result != 0) {
+                            return result;
+                        }  return o1.getAge()-o2.getAge();
+                    }
+                }
+               );
+        return o;
     }
 }
+
--- Trainee/Chapter_003_Collections_Lite/src/test/java/ua/ikonovalov/UserSortTest.java
+++ Trainee/Chapter_003_Collections_Lite/src/test/java/ua/ikonovalov/UserSortTest.java
@@ -1,4 +1,5 @@
 package ua.ikonovalov;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Set;
@@ -15,22 +16,59 @@ import static org.junit.Assert.assertThat;
  * @version 1.0
  */
 public class UserSortTest {
-    @Test
-    public void whenInArrayListToSortTreeSet() {
-        UserSort sort = new UserSort();
-        List<User> user = new ArrayList<>();
-        user.add(new User("Ivan", 22));
-        user.add(new User("Dima", 54));
+
+    private final UserSort sort = new UserSort();
+    private final List<User> user = new ArrayList<>();
+    private final List<User> expect = new ArrayList<>();
+
+    @Before
+    public void inputAndExpectUsers() {
+        user.add(new User("Ivanna", 22));
+        user.add(new User("Dmitriy", 54));
         user.add(new User("Petro", 17));
         user.add(new User("Inna", 21));
 
-        List<User> expect = new ArrayList<>();
+    }
+
+    /**
+     * Method add users to ArrayList converts to Set and sorts Age
+     */
+    @Test
+    public void whenInArrayListToSortAgeTreeSet() {
         expect.add(new User("Petro", 17));
         expect.add(new User("Inna", 21));
-        expect.add(new User("Ivan", 22));
-        expect.add(new User("Dima", 54));
+        expect.add(new User("Ivanna", 22));
+        expect.add(new User("Dmitriy", 54));
 
         assertThat(sort.sort(user).toString(), is(expect.toString()));
+    }
+
+    /**
+     * Method add users to ArrayList and sorts the length of the name
+     */
+    @Test
+    public void whenInArrayListToSortNameForLength() {
+        expect.add(new User("Inna", 21));
+        expect.add(new User("Petro", 17));
+        expect.add(new User("Ivanna", 22));
+        expect.add(new User("Dmitriy", 54));
+
+        assertThat(sort.sortNameLength(user).toString(), is(expect.toString()));
+    }
+
+    /**
+     * Method add users to ArrayList and sorts the name and after the Age
+     */
+    @Test
+    public void whenInArrayListToSortNameAndAge() {
+        user.add(new User("Dmitriy", 15));
+
+        expect.add(new User("Dmitriy", 15));
+        expect.add(new User("Dmitriy", 54));
+        expect.add(new User("Inna", 21));
+        expect.add(new User("Ivanna", 22));
+        expect.add(new User("Petro", 17));
+        assertThat(sort.sortByAllFields(user).toString(), is(expect.toString()));
     }
 
 }
