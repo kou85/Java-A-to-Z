a45de760b3307ef09d32e804af52187c34526a05
Ivan Konovalov <kou85@mail.ru>
2018-11-26 18:03:54 +0200

    process class ListCompare

--- Trainee/Chapter_003_Collections_Lite/src/main/java/ua/ikonovalov/ListCompare.java
+++ Trainee/Chapter_003_Collections_Lite/src/main/java/ua/ikonovalov/ListCompare.java
@@ -9,6 +9,17 @@ public class ListCompare implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        return 0;
+        int min = o1.length();
+            if (o1.length() > o2.length()) {
+                min = o2.length();
+            }
+            int res = 0;
+        for (int i = 0; i < min; i++) {
+            if(Character.compare(o1.charAt(i), o2.charAt(i)) != 0 ) {
+                res = Character.compare(o1.charAt(i), o2.charAt(i));
+                //break;
+                }
+            }
+        return res;
     }
 }
