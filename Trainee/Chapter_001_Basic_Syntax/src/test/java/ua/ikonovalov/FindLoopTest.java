8dff2884fc0ce292adf0df1b0f1c217b2e633c2f
Ivan Konovalov <kou85@mail.ru>
2018-11-27 15:34:15 +0200

    refactoring and testing Chapter_002_OOP

--- Trainee/Chapter_001_Basic_Syntax/src/test/java/ua/ikonovalov/FindLoopTest.java
+++ Trainee/Chapter_001_Basic_Syntax/src/test/java/ua/ikonovalov/FindLoopTest.java
@@ -24,7 +24,7 @@ public class FindLoopTest {
         int[] input = new int[] {5, 10, 3};
         int value = 2;
         int result = find.indexOf(input, value);
-        int expect = 0;
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
