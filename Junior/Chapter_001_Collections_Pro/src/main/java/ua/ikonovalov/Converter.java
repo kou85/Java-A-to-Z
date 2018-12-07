b6c5bbebaedbc0ebaf53c7ce0a6bd38ab83f4686
Ivan Konovalov <kou85@mail.ru>
2018-12-07 16:50:44 +0200

    new class Converter + Tests

---
+++ Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/Converter.java
@@ -0,0 +1,47 @@
+package ua.ikonovalov;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Strong on 07.12.18.
+ */
+public class Converter implements Iterator<Integer> {
+    /**
+     * Iterator of iterators
+     */
+    private Iterator<Iterator<Integer>> iterators;
+    /**
+     * instant selected iterator
+     */
+    private Iterator<Integer> instantIterator = null;
+
+
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+       this.iterators = it;
+        if (it.hasNext()) {
+            this.instantIterator = it.next();
+        }
+        return this;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return instantIterator.hasNext() || iterators.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        Integer result;
+        if (instantIterator.hasNext()) {
+            result = instantIterator.next();
+        } else if (iterators.hasNext()) {
+            instantIterator = iterators.next();
+            result = instantIterator.next();
+        } else {
+            throw new NoSuchElementException("No iterators");
+        }
+        return result;
+    }
+}
