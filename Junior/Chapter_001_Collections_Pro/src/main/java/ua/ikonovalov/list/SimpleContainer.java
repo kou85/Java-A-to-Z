d8174e72baf4ddac7102928471d3f986f0abf948
Ivan Konovalov <kou85@mail.ru>
2019-01-16 19:57:29 +0200

    validate

--- Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/list/SimpleContainer.java
+++ Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/list/SimpleContainer.java
@@ -3,7 +3,7 @@ package ua.ikonovalov.list;
 /**
  * Created by Strong on 13.01.19.
  */
-public interface SimpleContainer<E> extends Iterable<E>{
+public interface SimpleContainer<E> extends Iterable<E> {
     void add(E e);
     E get(int index);
 }
