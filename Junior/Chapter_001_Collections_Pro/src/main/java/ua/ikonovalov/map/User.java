c5f86ab44c69fb8c3e6ac21d3a2a0f74ddab366d
Ivan Konovalov <kou85@mail.ru>
2019-03-11 22:18:47 +0200

    class User

--- Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/map/User.java
+++ Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/map/User.java
@@ -1,6 +1,9 @@
 package ua.ikonovalov.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Strong on 30.01.19.
@@ -15,5 +18,18 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] arg) {
+        User user1 = new User("Tom", 2, new GregorianCalendar(2011, 07, 11));
+        User user2 = new User("Tom", 2, new GregorianCalendar(2011, 07, 11));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "one");
+        map.put(user2, "two");
+
+        System.out.println("Первый юзер: " + map.get(user1) + " Второй юзер: " + map.get(user2));
+        //Результат: выводит оба объекта т.к. хоть у них поля одинаковые но сами объекты разные.
+
+
     }
 }
