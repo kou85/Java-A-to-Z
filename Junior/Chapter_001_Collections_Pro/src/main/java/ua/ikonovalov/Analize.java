9f6aa539e90e26f7ae7c25493b0c557b4e0fa8e9
Ivan Konovalov <kou85@mail.ru>
2019-03-20 21:58:46 +0200

    class Analize

---
+++ Junior/Chapter_001_Collections_Pro/src/main/java/ua/ikonovalov/Analize.java
@@ -0,0 +1,100 @@
+package ua.ikonovalov;
+
+import java.util.List;
+
+/**
+ * Created by Strong on 20.03.19.
+ */
+public class Analize {
+
+
+
+    public Info diff(List<User> previous, List<User> current) {
+        List<User> prev;
+        List<User> cur;
+
+    }
+
+    public static class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = id;
+            result = 31 * result + (name != null ? name.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            User user = (User) o;
+
+            if (id != user.id) return false;
+            return name != null ? name.equals(user.name) : user.name == null;
+
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static class Info {
+
+        private int added;
+        private int changed;
+        private int deleted;
+
+        public Info(int added, int changed, int deleted) {
+            this.added = added;
+            this.changed = changed;
+            this.deleted = deleted;
+        }
+
+        public int getAdded() {
+            return added;
+        }
+
+        public int getChanged() {
+            return changed;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Info info = (Info) o;
+
+            if (added != info.added) return false;
+            if (changed != info.changed) return false;
+            return deleted == info.deleted;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = added;
+            result = 31 * result + changed;
+            result = 31 * result + deleted;
+            return result;
+        }
+    }
+}
