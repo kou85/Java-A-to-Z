12ddb2597f6fb0f5739f2ccdf301afcc50d68fcd
Ivan Konovalov <kou85@mail.ru>
2019-05-27 18:10:25 +0300

    process task #858 IO class Config

--- Junior/Chapter_002_Input-Output/src/main/java/ua/ikonovalov/Config.java
+++ Junior/Chapter_002_Input-Output/src/main/java/ua/ikonovalov/Config.java
@@ -1,9 +1,9 @@
 package ua.ikonovalov;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.StringJoiner;
 
 /**
  * Created by Strong on 10.04.19.
@@ -17,10 +17,44 @@ public class Config {
     }
 
     public void load() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("ua/ikonovalov/app.properties"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int character;
+        StringBuilder sb = new StringBuilder();
+        try {
+            while ((character = br.read()) != -1) {
+                char ch = (char) character;
+                if (character == ' ' || ch == '#' || ch == '=') break;
+                sb.append(ch);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
     public String value(String key) {
         throw new UnsupportedOperationException("Don't impl this method yet");
+    }
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
     }
 }
