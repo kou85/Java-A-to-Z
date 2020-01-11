2b56b29c090288b591fb2eadb882e3280a2b45a6
iv.konovalov <iv.konovalov@csltd.com.ua>
2020-01-11 17:21:48 +0200

    process class Search

---
+++ Junior/Chapter_002_Input-Output/src/main/java/ua/ikonovalov/Search.java
@@ -0,0 +1,12 @@
+package ua.ikonovalov;
+
+import java.io.File;
+import java.util.List;
+
+public class Search {
+    List<File> files(String parent, List<String> exts) {
+
+
+        return null;
+    }
+}
