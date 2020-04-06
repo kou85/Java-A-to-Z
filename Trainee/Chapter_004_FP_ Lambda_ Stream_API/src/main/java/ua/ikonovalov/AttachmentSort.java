6ed0e8bba19f3dbccff32c16bf5ee6bac3297029
Ivan Konovalov <kou85@mail.ru>
2020-04-06 23:20:33 +0300

    Method is sort String to descending used to Lambda

--- Trainee/Chapter_004_FP_ Lambda_ Stream_API/src/main/java/ua/ikonovalov/AttachmentSort.java
+++ Trainee/Chapter_004_FP_ Lambda_ Stream_API/src/main/java/ua/ikonovalov/AttachmentSort.java
@@ -19,8 +19,8 @@ public class AttachmentSort {
         Comparator comparator =  new Comparator() {
             @Override
             public int compare(Object o1, Object o2) {
-                Attachment left = o1;
-                Attachment right = o2;
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
                 return left.getSize() - right.getSize();
             }
         };
