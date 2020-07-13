ec0a6d4677d4c35f10e762348616c86f5e543bcc
Ivan Konovalov <kou85@mail.ru>
2020-07-13 23:07:46 +0300

    task Dir

---
+++ Junior/Chapter_002_Input-Output/src/main/java/ua/ikonovalov/Dir.java
@@ -0,0 +1,21 @@
+package ua.ikonovalov;
+
+import java.io.File;
+
+public class Dir {
+
+    public static void main(String[] args) {
+        File file = new File("d:\\java\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            if (subfile.isFile()) {
+                System.out.println("file name: " + subfile.getName() + " size: " + subfile.length());
+            }
+        }
+    }
+}
